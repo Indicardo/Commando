0ee0c87c7c6a2f8d0606a0dd405e16b585e6d41d
Indicado <indi88@riseup.net>
2022-04-03 19:37:13 +0200

    Overcast Update

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const fs = require('fs');
 const ws = require('ws');
@@ -13,7 +15,7 @@ const VALID_COLORS = ['#BE0039', '#FF4500', '#FFA800', '#FFD635', '#00A368', '#0
 var appData = {
     currentMap: 'blank.png',
     mapHistory: [
-        { file: 'blank.png', reason: 'Init ^Noah', date: 1648890843309 }
+        { file: 'blank.png', reason: 'Init ^Indicado', date: 164900695100 }
     ]
 };
 var brandUsage = {};
@@ -24,7 +26,7 @@ if (fs.existsSync(`${__dirname}/data.json`)) {
     appData = require(`${__dirname}/data.json`);
 }
 
-const server = app.listen(3987);
+const server = app.listen(4000);
 const wsServer = new ws.Server({ server: server, path: '/api/ws' });
 
 app.use('/maps', (req, res, next) => {
@@ -45,27 +47,27 @@ app.get('/api/stats', (req, res) => {
 
 app.post('/updateorders', upload.single('image'), async (req, res) => {
     if (!req.body || !req.file || !req.body.reason || !req.body.password || req.body.password !== process.env.PASSWORD) {
-        res.send('Ongeldig wachtwoord!');
+        res.send('Invalid password!');
         fs.unlinkSync(req.file.path);
         return;
     }
 
     if (req.file.mimetype !== 'image/png') {
-        res.send('Bestand moet een PNG zijn!');
+        res.send('File must be a PNG!');
         fs.unlinkSync(req.file.path);
         return;
     }
 
     getPixels(req.file.path, 'image/png', function (err, pixels) {
         if (err) {
-            res.send('Fout bij lezen bestand!');
+            res.send('Error reading file!');
             console.log(err);
             fs.unlinkSync(req.file.path);
             return
         }
 
         if (pixels.data.length !== 8000000) {
-            res.send('Bestand moet 2000x1000 zijn!');
+            res.send('File must be 2000x1000!');
             fs.unlinkSync(req.file.path);
             return;
         }
@@ -77,7 +79,7 @@ app.post('/updateorders', upload.single('image'), async (req, res) => {
 
             const hex = rgbToHex(r, g, b);
             if (VALID_COLORS.indexOf(hex) === -1) {
-                res.send(`Pixel op ${i % 2000}, ${Math.floor(i / 2000)} heeft ongeldige kleur.`);
+                res.send(`Pixel on ${i % 2000}, ${Math.floor(i / 2000)} has invalid color.`);
                 fs.unlinkSync(req.file.path);
                 return;
             }
